b801075f2fcaa425804d41b921851a5c4b070d08
Giv-18 <ourgnj@gmail.com>
2025-08-17 15:22:18 +0530

    Third Commit

---
+++ sw.js
@@ -0,0 +1,50 @@
+// service-worker.js
+
+const CACHE_NAME = "marauders-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/favicon.ico"
+];
+
+// Install event - cache assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event - serve from cache if available
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached response if found
+      if (response) {
+        return response;
+      }
+      // Otherwise fetch from network
+      return fetch(event.request);
+    })
+  );
+});
+
+// Activate event - remove old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log("Deleting old cache:", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
